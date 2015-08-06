4c823ebe97156cdf8cd9e00ee1aa45fbae9615f6
Florian LE BRENN <f.lebrenn@agence-belle-epoque.fr>
2015-08-06 18:16:54 +0000

    Add faculty admin page

--- sites/all/themes/faculty/templates/page.tpl.php
+++ sites/all/themes/faculty/templates/page.tpl.php
@@ -4,14 +4,22 @@
 			<?php if ($site_name || $logo): ?>
 				<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><?php print $site_name; ?></a>
 			<?php endif; ?>
+
+			<?php if ($main_menu): ?>
+				<nav>
+					<?php print theme('links__system_main_menu', array(
+						'links' => $main_menu,
+						'attributes' => array(
+							'id' => 'main-menu',
+							'class' => array('links', 'inline', 'clearfix')
+						),
+						//'heading' => t('Main menu')
+					)); ?>
+				</nav>
+			<?php endif; ?>
+
 			<?php print render($page['header']); ?>
 		</header>
-
-		<?php if ($main_menu): ?>
-		<nav>
-			<?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-		</nav>
-		<?php endif; ?>
 
 		<?php if ($breadcrumb): ?>
 		<div id="breadcrumb"><?php print $breadcrumb; ?></div>
@@ -31,7 +39,7 @@
 				<?php print render($page['help']); ?>
 				<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
 				<?php print render($page['content']); ?>
-				<?php print $feed_icons; ?>
+				<?php //print $feed_icons; ?>
 			</div>
 
 			<?php if ($page['sidebar_first']): ?>
