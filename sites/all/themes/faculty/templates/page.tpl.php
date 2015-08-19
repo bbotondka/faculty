1df21fc25e9bab4bf104a5c7f1cfece04dfdcb6f
Florian LE BRENN <florian.lebrenn@gmail.com>
2015-08-19 06:33:49 -0700

    Rm native main_menu in page.tpl.php. We use tb menu to diplay it

--- sites/all/themes/faculty/templates/page.tpl.php
+++ sites/all/themes/faculty/templates/page.tpl.php
@@ -9,19 +9,6 @@
 				<?php print $site_slogan; ?>
 			<?php endif; ?>
 
-			<?php if ($main_menu): ?>
-				<nav>
-					<?php print theme('links__system_main_menu', array(
-						'links' => $main_menu,
-						'attributes' => array(
-							'id' => 'main-menu',
-							'class' => array('links', 'inline', 'clearfix')
-						),
-						//'heading' => t('Main menu')
-					)); ?>
-				</nav>
-			<?php endif; ?>
-
 			<?php print render($page['header']); ?>
 		</header>
 
