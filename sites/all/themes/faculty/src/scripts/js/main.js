ff810d4efd2ab72a48fed657ed5556a230851d94
Botond Boross <bboross@pitech.fr>
2015-08-20 20:53:47 +0300

    First template commits for HP.

--- sites/all/themes/faculty/src/scripts/js/main.js
+++ sites/all/themes/faculty/src/scripts/js/main.js
@@ -1,5 +1,16 @@
-(function($) {
+(function ($) {
 
-	// all Javascript code goes here
+    // all Javascript code goes here
+    jQuery(document).ready(function () {
+        jQuery('.nav-trigger').click(function () {
+            if (jQuery(this).hasClass('active'))
+            {
+                jQuery('.nav-container').animate({'left': '-100%'}, 500);
+            } else {
+                jQuery('.nav-container').animate({'left': '0'}, 500);
+            }
+            jQuery(this).toggleClass('active');
+        })
+    })
 
 })(jQuery);
